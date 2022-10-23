03356bdb09fc4596128525c5a6296536285e43f3
JOPI-RTX3060\johan <johannes@walenta.dev>
2022-10-23 11:35:02 +0300

    experimenting with pos

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
-import React from "react";
-import { Canvas, extend } from "@react-three/fiber";
+import React, { useRef } from "react";
+import glsl from "babel-plugin-glsl/macro";
+import { Canvas, extend, useFrame } from "@react-three/fiber";
 import { useGLTF, OrbitControls, shaderMaterial } from "@react-three/drei";
 
 import "./App.css";
 
 const TreeShaderMaterial = shaderMaterial(
   // Uniform
-  {},
+  {
+    uTime: 0,
+  },
   // Vertex Shader
-  `  
-  void main() {   
-    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+  glsl`  
+  precision mediump float;
+
+  uniform float uTime;
+
+  #pragma glslify: snoise3 = require(glsl-noise/simplex/3d.glsl); 
+
+  void main() {  
+    vec3 pos = position;
+    float noiseFreq = 2.0;
+    float noiseAmp = 0.4;
+    vec3 noisePos = vec3(pos.x * noiseFreq + uTime, pos.y, pos.z);
+    pos.y += snoise3(noisePos) * noiseAmp;
+
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);  
   }`,
   // Fragment Shader
-  `  void main() {   
+  glsl` 
+   void main() {   
     gl_FragColor = vec4(0.194, 0.057, 0.025, 1.0);  
   }`
 );
 
 extend({ TreeShaderMaterial });
 
-
-
 const Tree = (props) => {
+  const ref = useRef();
   const { nodes, materials } = useGLTF("/baum.glb");
+
+  useFrame(({ clock }) => (ref.current.uTime = clock.getElapsedTime()));
 
   return (
     <group {...props} dispose={null}>
@@ -31,14 +48,14 @@ const Tree = (props) => {
         geometry={nodes.Cube759.geometry}
         material={materials["pine_tree_trunk.001"]}
       >
-        <treeShaderMaterial />
+        <treeShaderMaterial ref={ref} />
       </mesh>
-      <mesh
+      {/* <mesh
         castShadow
         receiveShadow
         geometry={nodes.Cube759_1.geometry}
         material={materials["tree_leaves.001"]}
-      />
+      /> */}
     </group>
   );
 };
