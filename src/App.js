dc768620ac18fac4e3602e76ebb18835e39b5978
JOPI-RTX3060\johan <johannes@walenta.dev>
2022-10-23 11:16:41 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Canvas, extend } from "@react-three/fiber";
+import { useGLTF, OrbitControls, shaderMaterial } from "@react-three/drei";
 
-function App() {
+import "./App.css";
+
+const TreeShaderMaterial = shaderMaterial(
+  // Uniform
+  {},
+  // Vertex Shader
+  `  
+  void main() {   
+    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+  }`,
+  // Fragment Shader
+  `  void main() {   
+    gl_FragColor = vec4(0.194, 0.057, 0.025, 1.0);  
+  }`
+);
+
+extend({ TreeShaderMaterial });
+
+
+
+const Tree = (props) => {
+  const { nodes, materials } = useGLTF("/baum.glb");
+
+  return (
+    <group {...props} dispose={null}>
+      <mesh
+        geometry={nodes.Cube759.geometry}
+        material={materials["pine_tree_trunk.001"]}
+      >
+        <treeShaderMaterial />
+      </mesh>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Cube759_1.geometry}
+        material={materials["tree_leaves.001"]}
+      />
+    </group>
+  );
+};
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas camera={{ position: [30, 5, 2], fov: 50 }}>
+        <mesh rotation={[-Math.PI / 2, 0, 0]}>
+          <planeBufferGeometry args={[100, 100]} />
+          <meshBasicMaterial color="limegreen" />
+        </mesh>
+
+        <Tree />
+
+        <OrbitControls />
+        <ambientLight args={[0xff0000]} intensity={0.1} />
+        <directionalLight position={[0, 0, 5]} intensity={0.5} />
+      </Canvas>
     </div>
   );
 }
-
-export default App;
